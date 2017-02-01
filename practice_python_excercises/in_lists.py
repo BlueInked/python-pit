387dfbcacc3cd7ab1a42ed47f9c6bd3a8771975a
BlueInked <BlueInked@users.noreply.github.com>
2017-02-01 22:06:27 +0300

    Exercise V version 1

---
+++ practice_python_excercises/in_lists.py
@@ -0,0 +1,35 @@
+import random
+
+def random_lists(items_in_list, stop_num):
+    ''' number -> list
+
+    Returns a random list of numbers in a given range
+
+    '''
+
+    my_random = []
+    
+    for i in range(int(items_in_list)):
+
+        my_random.append(random.randrange(1, int(stop_num),1))
+        
+        
+
+    print (my_random)
+
+    
+def overlap():
+    ''' list -> list 
+
+    Returns a new list of number if any included in two lists
+
+    '''
+    
+    in_both = []
+    
+            in_both.append(num)
+            
+    print(div)
+        
+
+
