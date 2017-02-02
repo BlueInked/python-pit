9f6b68cf5544b70b2fcb94a79d6d2d5965060423
BlueInked <BlueInked@users.noreply.github.com>
2017-02-02 21:31:16 +0300

    Exercise V ver 2

--- practice_python_excercises/in_lists.py
+++ practice_python_excercises/in_lists.py
@@ -1,35 +1,68 @@
 import random
+import operator
 
-def random_lists(items_in_list, stop_num):
+
+def random_list1():
+    ''' number -> list
+
+    Returns a random list of numbers in a given range
+
+    '''
+    
+    items_in_list1 = input("Give me the number of items you want in your list:")
+    stop_num1 = input("Give me the highest random number you would like:")
+
+    my_random1 = []
+
+
+    for i in range(int(items_in_list1)):
+
+        my_random1.append(random.randrange(1, int(stop_num1),1))
+    
+
+    print (my_random1)
+
+    
+def random_list2():
     ''' number -> list
 
     Returns a random list of numbers in a given range
 
     '''
 
-    my_random = []
-    
-    for i in range(int(items_in_list)):
+    items_in_list2 = input("Give me the another number of items you want in your list:")
+    stop_num2 = input("Give me the another highest random number you would like:")
 
-        my_random.append(random.randrange(1, int(stop_num),1))
+    my_random2 = []
+
+
+    for i in range(int(items_in_list2)):
+
+        my_random2.append(random.randrange(1, int(stop_num2),1))
         
-        
 
-    print (my_random)
+    print (my_random2)
+
+
 
     
-def overlap():
+def overlap(random1, random2):
     ''' list -> list 
 
     Returns a new list of number if any included in two lists
 
+
     '''
     
-    in_both = []
+    all_together = operator.add(random1, random2)
     
-            in_both.append(num)
-            
-    print(div)
-        
+    in_order = sorted(all_together, key = int)
+
+    in_both = list(set(in_order))
+
+    print(in_both)
+       
+
+
 
 
