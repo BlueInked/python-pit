b7e41e0c3032b31ccbe42672fd73203c82eb8127
BlueInked <BlueInked@users.noreply.github.com>
2017-02-05 19:59:42 +0300

    Exercise VII

---
+++ practice_python_excercises/comprehension.py
@@ -0,0 +1,19 @@
+
+
+
+def even_list(list_numbs):
+    ''' list -> list
+
+    Returns a list of even numbers in a given list
+
+    '''
+    evens = [num for num in list_numbs if num % 2 == 0]
+
+    print(evens)
+
+
+       
+
+
+
+
